7450f9157c611f8189832c2a0b54f64980f09ee0
Rami Karjalainen <rami.karjalainen@reaktor.fi>
2011-06-05 15:48:38 +0800

    Added some more functional c# thingies, to be used in presentation

---
+++ FunctionalTutorial/CSharpExamples/CSharpExamples/FunctionsAsCitizens.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSharpExamples
+{
+    public enum FunctionType
+    {
+        Combine,
+        Lambda,
+        StringCombine
+    }
+
+    public class FunctionsAsCitizens
+    {
+        public Func<int, int, int> GetFunctionOfType(FunctionType type)
+        {
+            // won't compile
+            // var func2 = Combine;
+            switch (type)
+            {
+                case FunctionType.Combine:
+                    return Combine;
+                case FunctionType.Lambda:
+                    return (a, b) => a*b;
+                case FunctionType.StringCombine:
+                    throw new NotImplementedException("not implemented");
+                default:
+                    throw new ArgumentOutOfRangeException("type");
+            }
+        }
+
+        private static int Combine(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
